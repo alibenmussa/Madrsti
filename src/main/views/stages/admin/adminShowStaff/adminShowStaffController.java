9e1a4f5b3056c5933d3a76c374b5396353b0258b
Yousef__breaka <yousefbreaka@gmail.com>
2019-02-28 16:32:32 +0200

    Adding : Admin and Employee home

--- src/main/views/stages/admin/adminShowStaff/adminShowStaffController.java
+++ src/main/views/stages/admin/adminShowStaff/adminShowStaffController.java
@@ -3,6 +3,9 @@ package main.views.stages.admin.adminShowStaff;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import main.views.dialog.Dialog;
 
@@ -13,7 +16,8 @@ public class adminShowStaffController implements Initializable {
     @FXML
     private TextField search;
 
-    @FXML
+
+
 
 
     @Override
