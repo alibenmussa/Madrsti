46308f59fa613541382e6a9e3565e62599a655c8
Yousef__breaka <yousefbreaka@gmail.com>
2019-03-14 02:36:42 +0200

    charts

--- src/main/views/stages/employee/employeeShowHome/employeeShowHomeController.java
+++ src/main/views/stages/employee/employeeShowHome/employeeShowHomeController.java
@@ -10,6 +10,7 @@ import main.DatabaseManager;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class employeeShowHomeController implements Initializable {
@@ -23,7 +24,7 @@ public class employeeShowHomeController implements Initializable {
     private Label subjects;
 
     @FXML
-    private LineChart<Number, Number> employeeChart;
+    private LineChart<String, Number> employeeChart;
 
 
     @Override
@@ -43,21 +44,44 @@ public class employeeShowHomeController implements Initializable {
     }
 
 
-
     public void getChartData() {
-        employeeChart.getData().clear();
-        XYChart.Series<Number, Number> series = new XYChart.Series<>();
-        series.setName("Series 1");
-        series.getData().add(new XYChart.Data<>(1, 20));
-        series.getData().add(new XYChart.Data<>(2, 100));
-        series.getData().add(new XYChart.Data<>(3, 80));
-        series.getData().add(new XYChart.Data<>(4, 180));
-        series.getData().add(new XYChart.Data<>(5, 20));
-        series.getData().add(new XYChart.Data<>(6, -10));
-        employeeChart.getData().add(series);
+        geyXYChartData(employeeChart);
 
     }
 
+    public static void geyXYChartData(LineChart<String, Number> employeeChart) {
+        String query = "SELECT grades.`name`, IFNULL(AVG(((first_midterm_exam + first_final_exam + second_midterm_exam + second_final_exam)"+
+                "/ full_mark) * 100), 0) FROM grades LEFT JOIN  subjects USING(grade_id) LEFT JOIN results USING(subject_id)"+
+                "GROUP BY subjects.`grade_id` ORDER BY grades.`name`";
+        ArrayList<String> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+
+        ResultSet rs = DatabaseManager.executeSQLResultSet(query,null);
+        try {
+
+        while (rs.next()) {
+
+            list1.add(rs.getString(1));
+            list2.add(rs.getInt(2));
+
+        }
+
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+
+        employeeChart.getData().clear();
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("Series 1");
+        series.getData().add(new XYChart.Data<>(list1.get(0), list2.get(0)));
+        series.getData().add(new XYChart.Data<>(list1.get(1), list2.get(1)));
+        series.getData().add(new XYChart.Data<>(list1.get(2), list2.get(2)));
+        series.getData().add(new XYChart.Data<>(list1.get(3), list2.get(3)));
+        series.getData().add(new XYChart.Data<>(list1.get(4), list2.get(4)));
+        series.getData().add(new XYChart.Data<>(list1.get(5), list2.get(5)));
+        employeeChart.setLegendVisible(false);
+        employeeChart.getData().add(series);
+    }
 
 
     private void result(String query, Label count,String label) {
